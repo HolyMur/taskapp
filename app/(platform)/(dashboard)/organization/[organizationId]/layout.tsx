049cdff20bbd8d0ece61e9ce2bbf107e92e00401
HolyMur <desolateyoursoul@gmail.com>
2024-01-03 23:07:46 +0300

    marketingpage, login/auth, navbar, sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,12 @@
+import {OrgControl} from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control";
+
+const OrganizationIdLayout = ({children}:{children: React.ReactNode}) => {
+    return (
+        <div>
+            <OrgControl/>
+            {children}
+        </div>
+    );
+};
+
+export default OrganizationIdLayout;
